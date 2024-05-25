00aa6d7fc98d8dbb476fba598847fde27ae7371f
Mounas82 <abdulrehman03082007@gmail.com>
2024-05-25 11:37:33 +0500

    fixed errors

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,7 @@ const Page = () => {
             <NavMenu text="ABOUT US" />
             <div className="flex justify-center gap-x-16 mt-20 mobile:flex-col mobile:items-center mobile:px-[10vw] mobile:gap-y-10 mb-20 mobile:mb-20">
                 <div className="w-[38%] flex flex-col gap-y-10 mt-5 mobile:w-full">
-                    <h1 className="text-5xl font-bold text-[#24F8C7] mobile:text-2xl">WE ARE THE WORLD'S LARGEST UNIVERSITY</h1>
+                    <h1 className="text-5xl font-bold text-[#24F8C7] mobile:text-2xl">WE ARE THE WORLD&apos;S LARGEST UNIVERSITY</h1>
                     <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cumque in accusantium molestias maiores fuga vitae eos, ducimus officiis repudiandae voluptatem error. Laboriosam, numquam blanditiis aspernatur, nobis tempora consequatur id aliquam asperiores voluptatum iste animi voluptatem fugiat totam excepturi dolor.</p>
                     <button className="border px-8 py-2 rounded-md border-purple-500 w-48 h-12 text-purple-500 hover:bg-[#24F8C7] hover:text-white hover hover:border-none transition-all duration-500 ease-in-out">EXPLORE NOW</button>
                 </div>
